d4f880a948ad40f632963f52d72f3ffab1565457
greysmm <greysmagnificentmonorepo@gmail.com>
2024-09-12 22:32:26 -0500

    router fix

--- apps/main/src/Landing.tsx
+++ apps/main/src/Landing.tsx
@@ -1,5 +1,5 @@
 import { Page } from "@greysmm.github.io/shared";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 const Landing = () => {
   return (
