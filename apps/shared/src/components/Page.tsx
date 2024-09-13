fe4f5dc9633276c2f7bc52cbf41ebc360d0cb619
greysmm <greysmagnificentmonorepo@gmail.com>
2024-09-12 23:05:03 -0500

    404 handling

--- apps/shared/src/components/Page.tsx
+++ apps/shared/src/components/Page.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
     { label: "About", link: "/#/about" },
   ];
   return (
-    <div className="flex pl-4 py-4 border border-white">
+    <div className="flex p-4 border border-white">
       <a href="https://github.com/greysmm/greysmm.github.io">
         <img
           src={GmmLogo}
@@ -22,7 +22,7 @@ const NavBar = () => {
       </a>
       <div className="flex justify-end w-full">
         {locArr.map((item, index) => (
-          <a className="mr-10 h-full flex items-center" key={"navitem_" + index} href={item.link}>
+          <a className="mx-4 h-full flex items-center" key={"navitem_" + index} href={item.link}>
             <div className="">{item.label}</div>
           </a>
         ))}
