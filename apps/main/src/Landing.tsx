3e09720d9d6fa15d8351d6f3c54d54640da471e6
greysmm <greysmagnificentmonorepo@gmail.com>
2024-09-12 23:09:08 -0500

    remove separate 404 page

--- apps/main/src/Landing.tsx
+++ apps/main/src/Landing.tsx
@@ -6,10 +6,9 @@ const Landing = () => {
     <Page>
     <Router>
     <Routes>
-      <Route path="/" element={<h2>Welcome to my website! More to come later...</h2>} />
       <Route path="/about" element={<h2>In progress...</h2>} />
       <Route path="/projects" element={<h2>No projects yet :(</h2>} />
-      <Route path="*" element={<h2>404 Error: page not found</h2>} />
+      <Route path="*" element={<h2>Welcome to my website! More to come later...</h2>} />
     </Routes>
   </Router>
   </Page>
