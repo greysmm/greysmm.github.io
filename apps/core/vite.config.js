18c42814bf0f53b8f39627540eae4444312982cc
greysmm <greysmagnificentmonorepo@gmail.com>
2024-09-14 20:47:24 -0500

    test alt domain

--- apps/core/vite.config.js
+++ apps/core/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hi/',
   plugins: [react()],
   css: {
     postcss: {
