c2143316cc8435e61133c32372a4ce32280a20ab
greysmm <greysmagnificentmonorepo@gmail.com>
2024-10-11 13:42:28 -0500

    added images to home page, made things look a bit better

--- packages/core/src/Landing.tsx
+++ packages/core/src/Landing.tsx
@@ -1,77 +1,81 @@
 import { Page } from "@greysmm/shared";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import landingImage from "./assets/landing.jpg";
+import aboutImage from "./assets/about.jpg";
+
+const BgDiv = ({ children, image }) => (
+  <div
+    className="w-full h-full flex flex-wrap gap-4 p-4 items-center justify-center bg-center"
+    style={{
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      backgroundImage: image ? `url(${image})` : "",
+    }}
+  >
+    {children}
+  </div>
+);
+
+const Text = ({ text }) => (
+  <div className="p-2 border-theme bg-theme">
+    <h2
+      style={window.innerWidth < 600 ? {} : { maxWidth: window.innerWidth / 2 }}
+    >
+      {text}
+    </h2>
+  </div>
+);
 
 const Landing = () => {
   return (
-    <Page>
-      <div className="w-full h-full flex items-center justify-center">
-        <Router>
-          <Routes>
-            <Route
-              path="/about"
-              element={
-                <h2
-                  style={
-                    window.innerWidth < 600
-                      ? {}
-                      : { maxWidth: window.innerWidth / 2 }
+    <Page className="p-0">
+      <Router>
+        <Routes>
+          <Route
+            path="/about"
+            element={
+              <BgDiv image={aboutImage}>
+                <Text
+                  text={
+                    "As mentioned on the home page, the purpose of this site is to display a few of the cool projects that I have been working on. Now, a bit about me, but not much for privacy reasons: I am relatively young, I live in the US, and as you may have guessed based on this site, I consider myself to be a computer scientist first and foremost. With that being said, I am something of a lifelong learner and often find myself wearing multiple hats; recently, I have been dabbling in data science and economics."
                   }
-                >
-                  As mentioned on the home page, the purpose of this site is to
-                  display a few of the cool projects that I have been working
-                  on. Now, a bit about me, but not much for privacy reasons: I
-                  am relatively young, I live in the US, and as you may have
-                  guessed based on this site, I consider myself to be a computer
-                  scientist first and foremost. With that being said, I am
-                  something of a lifelong learner and often find myself wearing
-                  multiple hats; recently, I have been dabbling in data science
-                  and economics.
-                </h2>
-              }
-            />
-            <Route
-              path="/projects"
-              element={
-                <>
-                  <a href="/starlight">
-                    <button className="btn text-lg border-theme p-4 mt-4">
-                      Project Starlight
-                      <span className="text-xs">
-                        <br />
-                        <br />
-                        Now with mobile support (laggy
-                        <br />
-                        until all stars are generated)!
-                      </span>
-                    </button>
-                  </a>
-                </>
-              }
-            />
-            <Route
-              path="*"
-              element={
-                <h2
-                  style={
-                    window.innerWidth < 600
-                      ? {}
-                      : { maxWidth: window.innerWidth / 2 }
+                />
+              </BgDiv>
+            }
+          />
+          <Route
+            path="/projects"
+            element={
+              <BgDiv image={""}>
+                <a href="/starlight">
+                  <button className="btn text-lg border-theme p-4 mt-4">
+                    Project Starlight
+                    <span className="text-xs">
+                      <br />
+                      <br />
+                      Now with mobile support (laggy
+                      <br />
+                      until all stars are generated)!
+                    </span>
+                  </button>
+                </a>
+              </BgDiv>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <BgDiv image={landingImage}>
+                <Text
+                  text={
+                    "Hello and welcome to the latest deployment of my magnificent monorepo (click the icon on the top left to see the code)! The idea is for me to have some place to display the cool projects that I have been working on in order to broaden my horizons and gain some more programming experience. Of course, I do have a life, so this will tend to get updated sporadically. Special thanks to Github for providing me all the tools to deploy my own website for free and with minimal hassle!"
                   }
-                >
-                  Hello and welcome to the latest deployment of my magnificent
-                  monorepo (click the icon on the top left to see the code)! The
-                  idea is for me to have some place to display the cool projects
-                  that I have been working on in order to broaden my horizons
-                  and gain some more programming experience. Of course, I do
-                  have a life, so this will tend to get updated sporadically.
-                  Special thanks to Github for providing me all the tools to
-                  deploy my own website for free and with minimal hassle!
-                </h2>
-              }
-            />
-          </Routes>
-        </Router>
-      </div>
+                />
+              </BgDiv>
+            }
+          />
+        </Routes>
+      </Router>
     </Page>
   );
 };
